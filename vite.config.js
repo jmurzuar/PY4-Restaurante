bac9cced5097a813f80f45e968112722a88eb260
jmurzuar <jmurzuar@gmail>
2024-03-12 01:40:45 -0300

    added 16 new react app

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PY4-Restaurante/',
-  
-  build: {
-    rollupOptions: {
-      external: [
-        '/PY4-Restaurante/imagen01.jpeg', // Agregar la ruta de la imagen externa aquí
-        '/PY4-Restaurante/imagen02.jpeg', // Agregar la ruta de la imagen externa aquí
-        '/PY4-Restaurante/imagen03.jpeg', // Agregar la ruta de la imagen externa aquí
-      ]
-    }
-  }
+  base: '/PY4-Restaurante/'
 
 })
 
