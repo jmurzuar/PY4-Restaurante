a7a542bbfe73b5e606e1f861b3e13a5909ccb85b
jmurzuar <jmurzuar@gmail>
2024-03-13 21:19:02 -0300

    added 25 new react app

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
     <BrowserRouter>
-      <App />
+    <HashRouter>
+    <App />
+    </HashRouter>
+      
     </BrowserRouter>
 
   </React.StrictMode>,
